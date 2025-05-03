587af7032888e3443015e48c17037045e0a06905
0x00satvara <physicyop@gmail.com>
2025-05-03 11:19:48 +0100

    pico

--- Level_2/picoshell/picoshell.c
+++ Level_2/picoshell/picoshell.c
@@ -7,49 +7,61 @@
 int picoshell(char **cmds[])
 {
     int res = 0;
-    int pr_fd = -1;
+    int prev_read_fd = -1;
+    int pipe_fd[2];
     pid_t pid;
     int i = 0;
-    int fd[2];
-    int sta;
+    pid_t child_pids[128];
+    int child_count = 0;
+
     while (cmds[i])
     {
-        if(cmds[i + 1])
-            pipe(fd);
+        if (cmds[i + 1] && pipe(pipe_fd) == -1)
+            return 1;
+
         pid = fork();
-        if( pid == -1)
+        if (pid == -1)
             return 1;
-        if (!pid)
+
+        if (pid == 0)
         {
-            if(pr_fd != -1)
+            if (prev_read_fd != -1)
             {
-                dup2(pr_fd, 0);
-                close(pr_fd);
+                dup2(prev_read_fd, STDIN_FILENO);
+                close(prev_read_fd);
             }
-            if(cmds[i + 1])
+
+            if (cmds[i + 1])
             {
-                dup2(fd[1], 1);
-                close(fd[1]);
+                dup2(pipe_fd[1], STDOUT_FILENO);
+                close(pipe_fd[1]);
+                close(pipe_fd[0]);
             }
+
             execvp(cmds[i][0], cmds[i]);
-            return 1;
+            exit(1);
         }
 
-        while (wait(&sta) == -1)
-        {
-            if(WIFEXITED(sta) && WEXITSTATUS(sta) != 0)
-                res = 1;
-        }
+        child_pids[child_count++] = pid;
 
-        if(pr_fd != -1)
-            close(pr_fd);
-
-        if(cmds[i + 1])
+        if (prev_read_fd != -1)
+            close(prev_read_fd);
+        if (cmds[i + 1])
         {
-            close(fd[1]);
-            pr_fd = fd[0];
+            close(pipe_fd[1]);
+            prev_read_fd = pipe_fd[0];
         }
         i++;
     }
+
+    int status;
+    for (int j = 0; j < child_count; j++)
+    {
+        if (waitpid(child_pids[j], &status, 0) == -1)
+            return 1;
+        else if (WIFEXITED(status) && WEXITSTATUS(status))
+            res = 1;
+    }
+
     return res;
 }
